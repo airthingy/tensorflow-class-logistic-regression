4074ad7eead6544bad2deca90baaf194c745e2c9
bibhas <bibhas2@gmail.com>
2019-08-12 09:38:43 -0400

    Use softmax for multi-class classification

--- linear_logistic_regression.py
+++ linear_logistic_regression.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import tensorflow.compat.v1 as tf
 
 def load_data(feature_file, label_file):
@@ -10,10 +11,13 @@ def load_data(feature_file, label_file):
     #Do one hot encoding: Convert 0 to [1, 0] and 1 to [0, 1]
     labels = pd.get_dummies(labels["Survived"]).values
 
-    return features, labels, features.shape[1]
+    return features, labels
 
-train_features, train_labels, n = load_data("train_features.csv", "train_labels.csv")
-test_features, test_labels, n = load_data("test_features.csv", "test_labels.csv")
+train_features, train_labels = load_data("train_features.csv", "train_labels.csv")
+test_features, test_labels = load_data("test_features.csv", "test_labels.csv")
+
+# Number of features
+n = train_features.shape[1]
 
 #Number of classes (survived, died)
 K = 2
@@ -32,41 +36,36 @@ W = tf.Variable(tf.zeros([n, K]))
 b = tf.Variable(tf.zeros([K])) 
 
 # Hypothesis 
-Y_hat = tf.nn.sigmoid(tf.add(tf.matmul(X, W), b)) 
+Y_hat =  tf.nn.softmax(tf.matmul(X, W) + b)
   
 # Sigmoid Cross Entropy Cost Function 
-cost = tf.nn.sigmoid_cross_entropy_with_logits( 
-                    logits = Y_hat, labels = Y) 
+cost = tf.reduce_mean(-tf.reduce_sum(Y*tf.log(Y_hat), reduction_indices=1))
   
 # Gradient Descent Optimizer 
 optimizer = tf.train.GradientDescentOptimizer(0.001).minimize(cost) 
 
+correct_prediction = tf.equal(tf.argmax(Y_hat, 1), tf.argmax(Y, 1)) 
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) 
+ 
 with tf.Session() as sess: 
       
     # Initializing the Variables 
     sess.run(tf.global_variables_initializer()) 
-      
-    # Lists for storing the changing Cost and Accuracy in every Epoch 
-    cost_history, accuracy_history = [], [] 
-      
+         
     # Iterating through all the epochs 
-    for epoch in range(10001): 
+    for epoch in range(50001): 
         cost_per_epoch = 0
           
         # Running the Optimizer 
         sess.run(optimizer, feed_dict = {X : train_features, Y : train_labels}) 
         
-        if epoch % 100 == 0:
-            
-            # Calculating accuracy on current Epoch 
-            correct_prediction = tf.equal(tf.argmax(Y_hat, 1), 
-                                            tf.argmax(Y, 1)) 
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, 
-                                                    tf.float32)) 
+        if epoch % 1000 == 0:
             # Calculating cost on current Epoch 
             current_cost = sess.run(cost, feed_dict = {X : train_features, Y : train_labels}) 
-            print(accuracy.eval({X : test_features, Y : test_labels}) * 100)
+            current_accuracy = sess.run(accuracy, feed_dict = {X : train_features, Y : train_labels}) 
+            print("Cost:", current_cost, "Accuracy:", current_accuracy * 100.0, "%")
 
-      
-    Weight = sess.run(W) # Optimized Weight 
-    Bias = sess.run(b)   # Optimized Bias 
+    
+    pred = sess.run(Y_hat, feed_dict = {X : test_features}) 
+    test_accuracy = sess.run(accuracy, feed_dict = {X : test_features, Y : test_labels})
+    print("Test accuracy:", test_accuracy * 100.0, "%")
