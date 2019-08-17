20cc21a3c15387e90df2918194ef5381e50b93d3
bibhas <bibhas2@gmail.com>
2019-08-17 13:03:57 -0400

    Deal with category features by creating indicator columns

--- prepare_data.py
+++ prepare_data.py
@@ -2,12 +2,13 @@ import pandas as pd
 
 read_training_cols = ["Survived","Pclass","Sex","Age","SibSp","Parch","Fare"]
 train_data = pd.read_csv('train.csv', usecols=read_training_cols)
-#Filter out rows missing age
-train_data = train_data[train_data.Age.notnull()]
+#Filter out rows missing data
+train_data = train_data.dropna()
 
-#Map male and female to numeric value. Female=1, Male=2
-train_data = train_data.replace({"Sex": {"female":1, "male":2}})
+#Replace categorical feature columns with indicator (dummy) columns
+train_data = pd.get_dummies(train_data, columns=["Pclass","Sex"], drop_first=True)
 
+#Separate the label column
 train_labels = train_data.pop("Survived")
 
 #Save processed data
@@ -20,14 +21,14 @@ train_labels.to_csv("train_labels.csv", header=True, index=False)
 
 read_test_cols = ["Pclass","Sex","Age","SibSp","Parch","Fare"]
 test_data = pd.read_csv('test.csv', usecols=read_test_cols)
-survival_data = pd.read_csv('gender_submission.csv')
+survival_data = pd.read_csv('gender_submission.csv', usecols=["Survived"])
 #Add the Survived column to test_data. We need to do that before we filter out bad rows 
 #that have missing data
-test_data["Survived"] = survival_data["Survived"]
-#Filter out rows missing age
-test_data = test_data[test_data.Age.notnull()]
-#Map male and female to numeric value. Female=1, Male=2
-test_data = test_data.replace({"Sex": {"female":1, "male":2}})
+test_data = pd.concat([test_data, survival_data], axis=1)
+#Filter out rows missing data
+test_data = test_data.dropna()
+#Replace categorical feature columns with indicator (dummy) columns
+test_data = pd.get_dummies(test_data, columns=["Pclass","Sex"], drop_first=True)
 
 test_labels = test_data.pop("Survived")
 
